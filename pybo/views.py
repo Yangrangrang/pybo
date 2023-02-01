90239c93b5bb77969ae7f04135e272f69eed5121
yangrangrang <gkssk2309@gmail.com>
2023-02-01 11:11:42 +0900

    질문등록

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionFrom
+
+def question_create(request):
+    '''질문 등록'''
+    print('request.method:{}'.format(request.method))
+    if request.method == 'POST':
+        # print('question_create post')
+        # 저장
+        form = QuestionFrom(request.POST)   # request.POST 데이터 (subject,content 자동 생성)
+        if form.is_valid(): # form(질문등록)이 유효하면
+            question = form.save(commit=False) # subject, content만 저장 (확정(commit)은 하지 않음)
+            question.create_date = timezone.now()
+            question.save() # 확정
+            return redirect("pybo:index")
+    else:
+        form = QuestionFrom()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html',context)
 
 '''bootstrap list'''
 def boot_menu(request):
