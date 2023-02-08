f9769c58d7cf1e8af6e127d5cae38bc430d38d75
yangrangrang <gkssk2309@gmail.com>
2023-02-08 12:37:49 +0900

    config

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from pybo import views
+from pybo.views import base_views
 
 from pybo.models import Question, Answer
 from django.utils import timezone
@@ -54,5 +54,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),
 ]
